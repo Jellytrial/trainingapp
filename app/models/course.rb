f07f0f89630f7c4ebb52d55a8c0a4484322a034f
Jellytrial <jelly.jelly@hotmail.com>
2020-05-25 17:25:37 +0900

    add course creation & edition error messages

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
+  validates :course_id, :course_title, :topic, :day_length, :level_id, :category, presence: true
   self.table_name = 'course' # use existing table
 end
